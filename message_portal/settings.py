778c18486735d6d3b0cea5bfc3f8d87fa03d1b41
Steve Solomon <steveskyler700@gmail.com>
2025-08-27 09:16:39 -0700

    allowed host set

--- message_portal/settings.py
+++ message_portal/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", 'nameofrender']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", 'facebook-login-s6oc.onrender.com']
 
 DATABASES = {
     "default": env.dj_db_url("DATABASE_URL")
